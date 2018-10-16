042cc671cbbb837d5811c3762a32936b52a39055
Gadiza Zerari <gadizaz@hotmail.com>
2018-10-16 16:32:32 +0100

    Test reading and printing elements passing

--- src/test/e2e/selenium.js
+++ src/test/e2e/selenium.js
@@ -1,10 +1,21 @@
 require('chromedriver');
 const seleniumDriver = require('selenium-webdriver');
+const Promise = require('bluebird')
 
 const driver = new seleniumDriver.Builder()
 .forBrowser('chrome')
 .build();
 
+const table = [];
+
+const tableRows = function (row, i) {
+  table[i] = [];
+  return row.findElements(seleniumDriver.By.tagName('td')).then(columns =>
+    // for every column
+    Promise.each(columns, (column, j) => columns[j].getText().then((number) => {
+      table[i][j] = parseInt(number);
+    })));
+};
 
 describe('Array challenge', () => {
   before(() => driver.get('http://localhost:3000'));
@@ -16,4 +27,10 @@ describe('Array challenge', () => {
   });
 
   it('Clicks button to render challenge', () => driver.findElement(seleniumDriver.By.xpath('//*[@id="home"]/div/div/button')).click());
-})
+
+  it('Reads the table elements', () => driver.findElements(seleniumDriver.By.tagName('tr'))
+      .then(rows => Promise.each(rows, tableRows)))
+
+
+  it('Prints the table', () => console.log(table));
+});
