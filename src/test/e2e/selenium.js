f6665bb07d9374e3f2000050f3bbd61b5f911ebb
Gadiza Zerari <gadizaz@hotmail.com>
2018-10-16 13:27:51 +0100

    Clicking button to render challenge test passing

--- src/test/e2e/selenium.js
+++ src/test/e2e/selenium.js
@@ -4,3 +4,16 @@ const seleniumDriver = require('selenium-webdriver');
 const driver = new seleniumDriver.Builder()
 .forBrowser('chrome')
 .build();
+
+
+describe('Array challenge', () => {
+  before(() => driver.get('http://localhost:3000'));
+
+  after(() => {
+    setTimeout(function () {
+      return driver.quit();
+    }, 5000);
+  });
+
+  it('Clicks button to render challenge', () => driver.findElement(seleniumDriver.By.xpath('//*[@id="home"]/div/div/button')).click());
+})
